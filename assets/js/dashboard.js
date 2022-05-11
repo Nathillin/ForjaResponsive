c8d444c5e63781bf51fc1aa2ddd20a6360298220
Nathalia Sarmiento <natysarmiento.10@gmail.com>
2022-05-11 18:08:21 -0500

    creation of modal for login page

---
+++ assets/js/dashboard.js
@@ -0,0 +1,110 @@
+// const formulario = document.getElementById('frm-bienvenida')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let button= document.querySelector("#buscar");
+
+function busqueda(){
+    let b= document.getElementById("texBuscar").value;
+
+    if(b == "52368795"){
+        alert("Caso1");
+    }
+
+    else if(b == "1001235478"){
+        alert("Caso2");
+    }
+
+    else if(b == "25413574"){
+        alert("Caso3");
+    }
+
+    else if(b == "1007203506"){
+        alert("Caso4");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// switch(b){
+//     case "52368795":
+//         alert("Caso1");
+//     break;
+
+//     case "1001235478":
+
+//         alert("Caso1");
+
+//     break;
+
+//     case "25413574":
+//         alert("Caso1");
+
+//     break;
+
+//     case "1007203506":
+//         alert("Caso1");
+//     break;
+
+// }
---
+++ assets/js/login.js
@@ -0,0 +1,27 @@
+let button= document.querySelector("#entrada");
+
+function ingresar(){
+    console.log("entre");
+    let c= document.getElementById("user").value;
+    let u= document.getElementById("password").value;
+
+    if (c =="Admin" && u == "Kuepa2022"){
+        document.querySelector(".form-login").setAttribute("action","dashboard.html");
+    }
+
+    if(c !="Admin" && u != "Kuepa2022"){
+        Swal.fire({
+            title: 'Credenciales incorrectas',
+            confirmButtonColor: '#c55110',
+            showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+            }
+        })
+    }
+}
+
+
+// window.setTimeout(()=>{window.location.href="login.html"},14000);
